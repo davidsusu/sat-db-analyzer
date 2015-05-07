8df1f6c528e0c63b44ac4c0ee235d69eeefbf0df
horvath <horvath@patronet.net>
2015-05-07 20:02:02 +0200

    Formula fully implemented

--- sat-db-analyzer/src/Formula.java
+++ sat-db-analyzer/src/Formula.java
@@ -1,19 +1,35 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
 public class Formula {
 
 	final private BigInteger number;
+
+	// TODO: TreeSet
+	private List<Clause> clausesCache = null;
 	
 	public Formula(BigInteger number) {
-		this.number = number.abs();
+		if (number.signum()==(-1)) {
+			throw new RuntimeException("Number must be non negative");
+		}
+		this.number = number;
 	}
 
 	public Formula(List<Clause> clauses) {
-		// TODO
-		this.number = null;
+		List<Clause> sortedClauses = new ArrayList<Clause>(clauses);
+		Collections.sort(sortedClauses, new ClauseSortComparator());
+		BigInteger two = new BigInteger("2");
+		BigInteger formulaNumber = new BigInteger("0");
+		for (Clause clause : sortedClauses) {
+			formulaNumber = formulaNumber.add(two.pow(clause.getNumber().intValue())); // FIXME: int! 
+		}
+		this.number = formulaNumber;
+		this.clausesCache = sortedClauses;
 	}
 
 	public Formula(Clause... clauses) {
@@ -25,13 +41,41 @@ public class Formula {
 	}
 	
 	public List<Clause> getClauses() {
-		// TODO
-		return null;
+		if (clausesCache==null) {
+			clausesCache = new ArrayList<Clause>();
+			int numberLength = number.bitLength(); // FIXME: int!
+			for (int i=0; i<numberLength; i++) {
+				if (number.testBit(i)) {
+					clausesCache.add(new Clause(new BigInteger(""+i)));
+				}
+			}
+		}
+		return clausesCache;
 	}
 	
 	public int getVariableCount() {
-		// TODO
-		return 1;
+		int variableCount = 0;
+		for (Clause clause: getClauses()) {
+			int greatestVariable = clause.getGreatestVariable();
+			if (greatestVariable>variableCount) {
+				variableCount = greatestVariable;
+			}
+		}
+		return variableCount;
+	}
+	
+	@Override
+	public String toString() {
+		return getClauses().toString();
+	}
+	
+	static public class ClauseSortComparator implements Comparator<Clause> {
+
+		@Override
+		public int compare(Clause clause1, Clause clause2) {
+			return clause1.getNumber().compareTo(clause2.getNumber());
+		}
+		
 	}
 	
 }
--- sat-db-analyzer/src/Main.java
+++ sat-db-analyzer/src/Main.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		String dbfile;
+		/*String dbfile;
 		if (args.length==0) {
 			System.out.print("Database file: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -20,8 +23,15 @@ public class Main {
 		} else {
 			dbfile = args[0];
 		}
-		Globals.put("dbfile", dbfile);
+		Globals.put("dbfile", dbfile);*/
+		
+		// FIXME
+		Formula formula1 = new Formula(Arrays.asList(new Clause[]{new Clause(-2, 4), new Clause(1, 2), new Clause(1, 3, -4)}));
+		System.out.println(formula1+" => "+formula1.getNumber());
 
+		Formula formula2 = new Formula(Arrays.asList(new Clause[]{new Clause(5)}));
+		System.out.println(formula2+" => "+formula2.getNumber());
+		
 		// TODO
 	}
 
