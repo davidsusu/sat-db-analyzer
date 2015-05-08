6040ad153cc1404e09d008935f597c8c09686b3b
horvath <horvath@patronet.net>
2015-05-08 18:47:53 +0200

    Minor improvements

--- sat-db-analyzer/src/Main.java
+++ sat-db-analyzer/src/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		/*String dbfile;
+		String dbfile;
 		if (args.length==0) {
 			System.out.print("Database file: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -22,29 +22,9 @@ public class Main {
 		} else {
 			dbfile = args[0];
 		}
-		Globals.put("dbfile", dbfile);*/
+		Globals.put("dbfile", dbfile);
 		
-		// FIXME
-		Formula[] formulas = new Formula[]{
-			new Formula(Arrays.asList(new Clause[]{new Clause()})),
-			new Formula(Arrays.asList(new Clause[]{new Clause(-2, 4), new Clause(1, 2), new Clause(1, 3, -4)})),
-			new Formula(Arrays.asList(new Clause[]{new Clause(1), new Clause(-1)})),
-			new Formula(Arrays.asList(new Clause[]{new Clause(2)})),
-		};
-		
-		FormulaValidator validator = new SolverFormulaValidator(100);
-		for (Formula formula : formulas) {
-			System.out.println("---------------------------------");
-			System.out.println(formula);
-			try {
-				System.out.println(validator.isSatisfiable(formula));
-			} catch (FormulaValidator.TimeoutException e) {
-				System.out.println("TIMEOUT");
-			}
-			
-		}
-		
-		// TODO
+		SqliteFormulaStorage.instance();
 	}
 
 }
--- sat-db-analyzer/src/SqliteFormulaStorage.java
+++ sat-db-analyzer/src/SqliteFormulaStorage.java
@@ -1,9 +1,14 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Iterator;
 
 
 public class SqliteFormulaStorage implements FormulaStorage {
+	
+	final public String TABLENAME = "formula";
 	
 	final private Connection sqliteConnection;
 	
@@ -17,7 +22,7 @@ public class SqliteFormulaStorage implements FormulaStorage {
 				connection = DriverManager.getConnection("jdbc:sqlite:"+Globals.get("dbfile"));
 			} catch (Exception e) {
 				// FIXME
-				throw new RuntimeException("Can not open database file");
+				throw new RuntimeException("Can not open database file", e);
 			}
 			_instance = new SqliteFormulaStorage(connection);
 		}
@@ -26,7 +31,11 @@ public class SqliteFormulaStorage implements FormulaStorage {
 	
 	public SqliteFormulaStorage(Connection sqliteConnection) {
 		this.sqliteConnection = sqliteConnection;
-		// TODO: init database
+		try {
+			init();
+		} catch (SQLException e) {
+			throw new RuntimeException("Can not initalize database file", e);
+		}
 	}
 	
 	@Override
@@ -44,6 +53,17 @@ public class SqliteFormulaStorage implements FormulaStorage {
 	public Iterator<Row> iterator() {
 		// TODO
 		return null;
+	}
+	
+	protected void init() throws SQLException {
+		PreparedStatement statement = sqliteConnection.prepareStatement("SELECT name FROM sqlite_master WHERE type='table' AND name=?");
+		statement.setString(0, TABLENAME);
+		ResultSet resultSet = statement.executeQuery();
+		if (resultSet.isAfterLast()) {
+			PreparedStatement createStatement = sqliteConnection.prepareStatement("CREATE TABLE ? ()");
+			createStatement.setString(0, TABLENAME);
+			createStatement.execute();
+		}
 	}
 	
 }
